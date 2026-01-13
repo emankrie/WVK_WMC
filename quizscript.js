3b7e3269c0c2d3b3053c0f4023e7405349d5d347
emankrie <krieger.emanuel@yahoo.de>
2026-01-13 15:45:51 +0100

    filling the js with questions and answers

---
+++ quizscript.js
@@ -0,0 +1,242 @@
+const questions = [
+    {
+        question: "Wann wurde der FC Bayern München gegründet?",
+        answers: [
+            {answer1: "1900", correct: true},
+            {answer2: "1920", correct: false},
+            {answer3: "1932", correct: false},
+        ]
+    },
+    {
+        question: "Wie lautet der Spitzname des Vereins?",
+        answers: [
+            {answer1: "Die Roten", correct: true},
+            {answer2: "Die Blauen", correct: false},
+            {answer3: "Die Schwarzen", correct: false},
+        ]
+    },
+    {
+        question: "In welchem Stadion trägt der FC Bayern seine Heimspiele aus?",
+    answers: [
+            {answer1: "Allianz Arena", correct: true},
+            {answer2: "Olympiastadion München", correct: false},
+            {answer3: "Signal Iduna Park", correct: false},
+        ]
+    },
+    {
+        question: "Wer war der erste Trainer des FC Bayern?",
+    answers: [
+            {answer1: "Udo Lattek", correct: false},
+            {answer2: "Richard Dombi", correct: true},
+            {answer3: "Dettmar Cramer", correct: false},
+        ]
+    },
+    {
+        question: "Wer war der erste deutsche Weltfußballer aus Bayern?",
+    answers: [
+            {answer1: "Franz Beckenbauer", correct: false},
+            {answer2: "Lothar Matthäus", correct: true},
+            {answer3: "Gerd Müller", correct: false},
+        ]
+    },
+    {
+        question: "Welcher Spieler gilt als „Bomber der Nation“?",
+    answers: [
+            {answer1: "Karl-Heinz Rummenigge", correct: false},
+            {answer2: "Gerd Müller", correct: true},
+            {answer3: "Franz Beckenbauer", correct: false},
+        ]
+    },
+    {
+        question: "Wie viele Champions-League-Titel hat Bayern (Stand 2025)?",
+    answers: [
+            {answer1: "5", correct: false},
+            {answer2: "6", correct: true},
+            {answer3: "4", correct: false},
+        ]
+    },
+    {
+        question: "Wer war der Gegner im Champions-League-Finale 2013?",
+    answers: [
+            {answer1: "Real Madrid", correct: false},
+            {answer2: "Chelsea FC", correct: false},
+            {answer3: "Borussia Dortmund", correct: true},
+        ]
+    },
+    {
+        question: "Welche Vereinsfarben hat der FC Bayern?",
+    answers: [
+            {answer1: "Blau und Gelb", correct: false},
+            {answer2: "Rot und Weiß", correct: true},
+            {answer3: "Grün und Schwarz", correct: false},
+        ]
+    },
+    {
+        question: "Welches Spitzname-Duo prägte den FC Bayern über Jahre?",
+    answers: [
+            {answer1: "Magic-Müller", correct: false},
+            {answer2: "Killer-Kalle", correct: false},
+            {answer3: "Robbery ", correct: true},
+        ]
+    },
+    {
+        question: "Wer war der langjährige Präsident des Vereins?",
+    answers: [
+            {answer1: "Uli Hoeneß", correct: true},
+            {answer2: "Franz Beckenbauer", correct: false},
+            {answer3: "Karl Hopfner", correct: false},
+        ]
+    },
+    {
+        question: "Welcher Spieler trug die Nummer 25 und wurde zur Legende?",
+    answers: [
+            {answer1: "Bastian Schweinsteiger", correct: false},
+            {answer2: "Thomas Müller", correct: true},
+            {answer3: "Philipp Lahm", correct: false},
+        ]
+    },
+    {
+        question: "",
+    answers: [
+            {answer1: "", correct: true},
+            {answer2: "", correct: false},
+            {answer3: "", correct: false},
+        ]
+    },
+    {
+        question: "Wie heißt das Maskottchen des FC Bayern?",
+    answers: [
+            {answer1: "Fritzi", correct: false},
+            {answer2: "Leo", correct: false},
+            {answer3: "Berni", correct: true},
+        ]
+    },
+    {
+        question: "Welcher Klub ist der größte Rivale in Deutschland?",
+    answers: [
+            {answer1: "Schalke 04", correct: false},
+            {answer2: "Borussia Dortmund", correct: true},
+            {answer3: "Hamburger SV", correct: false},
+        ]
+    },
+    {
+        question: "Wie viele Deutsche Meisterschaften hat Bayern (Stand 2025)?",
+    answers: [
+            {answer1: "30", correct: false},
+            {answer2: "32", correct: true},
+            {answer3: "28", correct: false},
+        ]
+    },
+    {
+        question: "Wer war Kapitän beim Champions-League-Sieg 2001?",
+    answers: [
+            {answer1: "Oliver Kahn", correct: false},
+            {answer2: "Stefan Effenberg", correct: true},
+            {answer3: "Lothar Matthäus", correct: false},
+        ]
+    },
+    {
+        question: "Welcher Spieler kam 2023 von Tottenham Hotspur?",
+    answers: [
+            {answer1: "Harry Kane", correct: true},
+            {answer2: "Son Heung-min", correct: false},
+            {answer3: "Dele Alli", correct: false},
+        ]
+    },
+    {
+        question: "Wer war der Trainer beim Triple 2013?",
+    answers: [
+            {answer1: "Ottmar Hitzfeld", correct: false},
+            {answer2: "Pep Guardiola", correct: false},
+            {answer3: "Jupp Heynckes", correct: true},
+        ]
+    },{
+        question: "Welche Stadt ist die Heimat des FC Bayern?",
+    answers: [
+            {answer1: "Nürnberg", correct: false},
+            {answer2: "München", correct: true},
+            {answer3: "Augsburg", correct: false},
+        ]
+    },
+    {
+        question: "Wer war der Gegner im „Finale dahoam“ 2012?",
+    answers: [
+            {answer1: "Real Madrid", correct: false},
+            {answer2: "Inter Mailand", correct: false},
+            {answer3: "Chelsea FC", correct: true},
+        ]
+    },
+    {
+        question: "Welcher Spieler war bekannt für seine Flanken von rechts?",
+    answers: [
+            {answer1: "Franck Ribéry", correct: false},
+            {answer2: "Arjen Robben", correct: true},
+            {answer3: "Philipp Lahm", correct: false},
+        ]
+    },
+    {
+        question: "Wie heißt die Vereins-Hymne?",
+    answers: [
+            {answer1: "Forever Bayern", correct: false},
+            {answer2: "Mia san mia", correct: false},
+            {answer3: "Stern des Südens", correct: true},
+        ]
+    },
+    {
+        question: "Wer war der erste ausländische Trainer des Vereins?",
+    answers: [
+            {answer1: "Zlatko Čajkovski", correct: true},
+            {answer2: "Giovanni Trapattoni", correct: false},
+            {answer3: "Louis van Gaal", correct: false},
+        ]
+    },
+    {
+        question: "Welcher Spieler erzielte das Siegtor im CL-Finale 2013?",
+    answers: [
+            {answer1: "Mario Mandžukić", correct: false},
+            {answer2: "Arjen Robben", correct: true},
+            {answer3: "Thomas Müller", correct: false},
+        ]
+    },
+    {
+        question: "Wie viele Zuschauer fasst die Allianz Arena ungefähr?",
+    answers: [
+            {answer1: "80.000", correct: false},
+            {answer2: "60.000", correct: false},
+            {answer3: "75.000", correct: true},
+        ]
+    },
+    {
+        question: "Wer war der Torwart-Legende mit Spitznamen „Titan“?",
+    answers: [
+            {answer1: "Oliver Kahn", correct: true},
+            {answer2: "Sepp Maier", correct: false},
+            {answer3: "Manuel Neuer", correct: false},
+        ]
+    },
+    {
+        question: "Welcher Spieler war bekannt für den „linken Hammer“?",
+    answers: [
+            {answer1: "Franck Ribéry", correct: false},
+            {answer2: "Bastian Schweinsteiger", correct: true},
+            {answer3: "Mehmet Scholl", correct: false},
+        ]
+    },
+    {
+        question: "Wer war der Trainer nach Jupp Heynckes 2013?",
+    answers: [
+            {answer1: "Carlo Ancelotti", correct: false},
+            {answer2: "Louis van Gaal", correct: false},
+            {answer3: "Pep Guardiola", correct: true},
+        ]
+    },
+    {
+        question: "Welche Zahl trägt Manuel Neuer auf dem Trikot?",
+    answers: [
+            {answer1: "12", correct: false},
+            {answer2: "22", correct: false},
+            {answer3: "1", correct: true},
+        ]
+    },
+
+]
